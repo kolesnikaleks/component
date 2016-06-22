d3d1f2472d1ce415388067a1d7fbbabb5c64bd7f
kolesnikaleks <dj.bond@mail.ru>
2016-06-22 11:08:08 +0300

    Add new table #__comparison

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -26,6 +26,21 @@ CREATE TABLE `#__properties` (
 	AUTO_INCREMENT =0
 	DEFAULT CHARSET =utf8;
 
+DROP TABLE IF EXISTS `#__comparison`;
+
+CREATE TABLE `#__properties` (
+	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
+		`prop_name` VARCHAR(25) NOT NULL,
+		`description` VARCHAR(25) NOT NULL,
+		`value` VARCHAR(25) NOT NULL,
+			`published` tinyint(4) NOT NULL,
+	`catid` VARCHAR(25) NOT NULL,
+	PRIMARY KEY (`id`)
+)
+	ENGINE =MyISAM
+	AUTO_INCREMENT =0
+	DEFAULT CHARSET =utf8;
+
 
 
 
