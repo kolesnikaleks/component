b82620fbe34290ad2a9322c7e13008c33cda6d82
kolesnikaleks <dj.bond@mail.ru>
2016-06-22 14:52:28 +0300

    Manager add class

--- admin/controllers/manager.php
+++ admin/controllers/manager.php
@@ -5,7 +5,7 @@ defined('_JEXEC') or die;
 
 class ComparisonControllerManager extends JControllerAdmin
 {
-    public function getModel($name = 'Manage', $prefix = 'ComparisonModel', $config = array('ignore_request' => true))
+    public function getModel($name = 'Manager', $prefix = 'ComparisonModel', $config = array('ignore_request' => true))
     {
         $model = parent::getModel($name, $prefix, $config);
 
