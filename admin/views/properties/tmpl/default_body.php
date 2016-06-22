9440812bf098a4f4b6f37b421938488e017bdd08
kolesnikaleks <dj.bond@mail.ru>
2016-06-22 12:44:44 +0300

    Change body_view_padding

--- admin/views/properties/tmpl/default_body.php
+++ admin/views/properties/tmpl/default_body.php
@@ -4,27 +4,27 @@ defined('_JEXEC') or die('Restricted Access');
 ?>
 <?php foreach ($this->itemsis as $i => $item):
     $link = JRoute::_('index.php?option=com_comparison&task=property.edit&id=' . $item->id); ?>
-    <tr class="row_<?php echo $i % 2; ?>" style="border-bottom: 1px solid ">
+    <tr class="<?php echo $i % 2; ?>" >
         <td>
             <?php echo $item->id; ?>
         </td>
-        <td style="padding-left: 18px;">
+        <td >
             <?php echo JHtml::_('grid.id', $i, $item->id); ?>
         </td>
 
-        <td style="padding:20px 50px;">
+        <td >
             <a href="<?php echo $link ?>">
                 <?php echo $item->prop_name; ?>
             </a>
         </td>
-        <td style="padding:20px 30px;">
+        <td >
             <?php echo $item->value; ?>
         </td>
-        <td style="padding:20px 70px;">
+        <td >
             <?php echo $item->description; ?>
         </td>
 
-        <td style="padding:20px 30px;">
+        <td >
             <?php echo $item->title; ?>
         </td>
 
