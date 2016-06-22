dfbd10b65905d25a0feb568bc6a778b8dbd1c0bb
kolesnikaleks <dj.bond@mail.ru>
2016-06-22 12:45:29 +0300

    Change_head_padding

--- admin/views/properties/tmpl/default_head.php
+++ admin/views/properties/tmpl/default_head.php
@@ -4,23 +4,23 @@ defined('_JEXEC') or die('Restricted Access');
 ?>
 
 <tr>
-    <th width="10" style="margin-left: ;">
+    <th  >
         <?php echo JText::_('COM_COMPARISON_PROPERTIES_HEADING_ID'); ?>
     </th>
-    <th width="50" >
+    <th  >
         <?php echo JHtml::_('grid.checkall'); ?>
     </th>
 
-    <th style="">
+    <th >
         <?php echo JText::_('COM_COMPARISON_PROPERTIES_HEADING_TITLE'); ?>
     </th>
-    <th style="">
+    <th >
         <?php echo JText::_('COM_COMPARISON_PROPERTIES_HEADING_VALUE'); ?>
     </th>
-    <th style="">
+    <th >
         <?php echo JText::_('COM_COMPARISON_PROPERTIES_HEADING_DESCRIPTION'); ?>
     </th>
-    <th style="">
+    <th >
         <?php echo JText::_('COM_COMPARISON_PROPERTIES_HEADING_CATID'); ?>
     </th>
 
