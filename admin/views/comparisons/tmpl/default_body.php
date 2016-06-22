54011269488f1afe4650ca9a1d06a074929c1dc0
kolesnikaleks <dj.bond@mail.ru>
2016-06-22 12:49:14 +0300

    change body view padding comparisons

--- admin/views/comparisons/tmpl/default_body.php
+++ admin/views/comparisons/tmpl/default_body.php
@@ -4,24 +4,24 @@ defined('_JEXEC') or die('Restricted Access');
 ?>
 <?php foreach ($this->items as $i => $item):
     $link = JRoute::_('index.php?option=com_comparison&task=comparison.edit&id=' . $item->id);?>
-    <tr class="row-fluid<?php echo $i % 2; ?>" style="border-bottom: 1px solid ">
+    <tr class="<?php echo $i % 2; ?>" >
         <td>
             <?php echo $item->id; ?>
         </td>
-        <td style="padding-left: 18px;">
+        <td >
             <?php echo JHtml::_('grid.id', $i, $item->id); ?>
         </td>
 
-        <td style="padding:20px 50px;">
+        <td >
 <a href="<?php echo $link ?>">
             <?php echo $item->name; ?>
         </a>
         </td>
 
-        <td style="padding:20px 30px;">
+        <td >
             <?php echo $item->title; ?>
         </td>
-        <td style="padding:20px 70px;">
+        <td >
             <?php echo $item->status; ?>
         </td>
 
