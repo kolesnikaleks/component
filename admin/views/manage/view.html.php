a0e4980f164d9ad944feca7239a757e2fdb04d88
kolesnikaleks <dj.bond@mail.ru>
2016-06-24 17:32:20 +0300

    view update

--- admin/views/manage/view.html.php
+++ admin/views/manage/view.html.php
@@ -3,42 +3,59 @@
 defined('_JEXEC') or die('Restricted access');
 // import Joomla view library
 jimport('joomla.application.component.view');
+
 /**
- * HelloWorlds View
+ * Properties
  */
-class ComparisonViewManager extends JViewLegacy
+class ComparisonViewManage extends JViewLegacy
 {
+
+
+
+
     function display($tpl = null)
     {
         // Get data from the model
-        $items      = $this->get('Items');
+        $items = $this->get('Items');
+
+
+        $property = $this->get('Property');
+
+
         $pagination = $this->get('Pagination');
         // Check for errors.
-        if (count($errors = $this->get('Errors')))
-        {
+        if (count($errors = $this->get('Errors'))) {
             JError::raiseError(500, implode('<br />', $errors));
             return false;
         }
         // Assign data to the view
-        $this->items      = $items;
+
+        $this->items = $items;
+
+
+        $this->property = $property;
+
+
         $this->pagination = $pagination;
+
 
         // Set the toolbar
         $this->addToolBar();
         // Display the template
         parent::display($tpl);
 
-        ComparisonHelper::addSubmenu('manager');
+        ComparisonHelper::addSubmenu('manage');
 
     }
+
     /**
      * Setting the toolbar
      */
     protected function addToolBar()
     {
-        JToolBarHelper::title(JText::_('COM_COMPARISON_MANAGER_COMPARISON'));
-        JToolBarHelper::deleteList('', 'comparisons.delete');
-        JToolBarHelper::editList('comparison.edit');
-        JToolBarHelper::addNew('comparison.add');
+        JToolBarHelper::title(JText::_('COM_COMPARISON_MANAGER_PROPERTIES'));
+        JToolBarHelper::deleteList('', 'properties.delete');
+        JToolBarHelper::save('property.edit');
+        JToolBarHelper::addNew('property.add');
     }
 }
